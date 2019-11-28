8aebef42548a0f5617d316516548aa64cd28b694
Yuri Malyshev <yurimalyshev.gm@gmail.com>
2019-11-28 17:40:19 +0300

    Lesson 6 assignment added.

---
+++ Assets/Scripts/Core/LessonsInternal/Lesson06_01_Internal.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace KarelTheRobotUnity.Core
+{
+    public abstract class Lesson06_01_Internal : SyncActionSceneController
+    {
+        public enum BeeperType
+        {
+            Undefined = 0,
+            Blue = 1,
+            Red = 2
+        }
+
+        private List<Beeper> _randomizedBeepers;
+        private BeeperType _deliveredBeepersType;
+        
+        public List<Beeper> GetBeepers()
+        {
+            return _randomizedBeepers;
+        }
+
+        public BeeperType GetDeliveredBeepersType()
+        {
+            return _deliveredBeepersType;
+        }
+
+        public void OnDeliverRedBeepersButtonClick()
+        {
+            _deliveredBeepersType = BeeperType.Red;
+            Execute();
+        }
+
+        public void OnDeliverBlueBeepersButtonClick()
+        {
+            _deliveredBeepersType = BeeperType.Blue;
+            Execute();
+        }
+
+        protected abstract void Execute();
+
+        protected override void Program()
+        {
+            RandomizeBeepers();
+            RandomizeRobotsRotations();
+        }
+
+        private void RandomizeBeepers()
+        {
+            var field = FindObjectOfType<Field>();
+            
+            int beepersCount = Random.Range(4, 7);
+            var set = new HashSet<Vector2Int>();
+            var beepers = new List<Beeper>(beepersCount);
+
+            for (int i = 0; i < beepersCount; ++i)
+            {
+                bool emptyCellUsed = false;
+                while (!emptyCellUsed)
+                {
+                    var randomizedCoordinates = new Vector2Int(Random.Range(1, field.Size.x - 1), Random.Range(1, field.Size.y - 6));
+                    if (set.Add(randomizedCoordinates))
+                    {
+                        var cell = field.GetCell(randomizedCoordinates);
+                        var beeper = Instantiate(MainSettings.Instance.MainBeeperPrefab, cell.transform);
+                        cell.IsBeeperPresentedSync = true;
+                        beeper.SetColor(Random.Range(0, 2) == 1 ? Color.red : Color.blue);
+                        beeper.Position = randomizedCoordinates;
+                        beepers.Add(beeper);
+                        emptyCellUsed = true;
+                    }
+                }
+            }
+
+            _randomizedBeepers = beepers;
+        }
+
+        private void RandomizeRobotsRotations()
+        {
+            var directions = Enum.GetValues(typeof(Direction))
+                .OfType<Direction>()
+                .ToList()
+                .FindAll(v => v != Direction.Undefined);
+
+            foreach (var robot in Robots)
+            {
+                robot.SetRotationImmediately(directions[Random.Range(0, directions.Count)]);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Lessons/Lesson06_01.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using KarelTheRobotUnity.Core;
+using UnityEngine;
+using BeeperType = KarelTheRobotUnity.Core.Lesson06_01_Internal.BeeperType;
+
+namespace TestNamespace
+{
+    public class Lesson06_01 : Lesson06_01_Internal
+    {
+        protected override void Execute()
+        {
+            // В данном задании необходимо в зависимости от нажатой кнопки развезти либо красные биперы, либо синие.
+            // Синие биперы необходимо складывать по оси Y друг за другом начиная с верхнего левого угла, красные - из правого верхнего угла по диагонали.
+            // 
+            // Алгоритмы развоза должен быть реализованы в двух разных классах, BlueBeepersDeliveryStrategy и RedBeepersDeliveryStrategy, которые должны передаваться в вызывающий их
+            // класс RobotStrategyExecutor, наследник SmartRobot, написанного на прошлом занятии.
+            // Эти классы должны наследоваться от абстрактного класса RobotDeliveryStrategy и переопределять виртуальный метод Execute(RobotStrategyExecutor robot).
+            //
+            // Начальный поворот робота и положения биперов рандомные
+            //
+            // Необходимый код:
+            // проверка бипера: if (beeper.GetColor() == Color.red) или if (beeper.GetColor() == Color.blue)
+            // 
+
+            List<Beeper> beepers = GetBeepers();
+            BeeperType beepersTypeToDeliver = GetDeliveredBeepersType();
+            var mainRobot = MainRobot;
+
+            switch (beepersTypeToDeliver)
+            {
+                case BeeperType.Red:
+                    Debug.Log("Delivering red beepers");
+                    break;
+                case BeeperType.Blue:
+                    Debug.Log("Delivering blue beepers");
+                    break;
+                default:
+                    throw new System.Exception($"Unknown beeper type = {beepersTypeToDeliver}");
+            }
+        }
+    }
+
+    public class SmartRobot
+    {
+        private RobotSyncActionWrapper _robot;
+
+        public SmartRobot(RobotSyncActionWrapper robot)
+        {
+            _robot = robot;
+        }
+
+        public void DeliverBeeperToTargetLocation(Beeper beeper, Vector2Int targetLocation)
+        {
+            // 1) Доехать до бипера
+            TravelToLocation(beeper.Position);
+
+            // 2) Поднять бипер
+            _robot.TakeBeeper();
+
+            // 3) Доехать до цели
+            TravelToLocation(targetLocation);
+
+            // 4) Положить бипер
+            _robot.PutBeeper();
+        }
+
+        public void TravelToLocation(Vector2Int targetLocation)
+        {
+            var difference = targetLocation - _robot.Position;
+
+            var xDirection = difference.x > 0 ? Direction.East : Direction.West;
+            TurnToDirection(xDirection);
+            MoveSteps(Math.Abs(difference.x));
+
+            var yDirection = difference.y > 0 ? Direction.North : Direction.South;
+            TurnToDirection(yDirection);
+            MoveSteps(Math.Abs(difference.y));
+        }
+
+        public void TurnToDirection(Direction direction)
+        {
+            while (_robot.Rotation != direction)
+            {
+                _robot.TurnRight();
+            }
+        }
+
+        private void MoveSteps(int steps)
+        {
+            for (int i = 0; i < steps; ++i)
+            {
+                _robot.Move();
+            }
+        }
+    }
+
+    
+}
